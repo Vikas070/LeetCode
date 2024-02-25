c710f47a2f0d6d84a1f741e34d45e521a44c9d69
Vikas Saxena <40655200+Vikas070@users.noreply.github.com>
2024-02-25 05:34:37 -0800

    Time: 69 ms (76.82%) | Memory: 53 MB (26.49%) - LeetSync

--- 383-ransom-note/ransom-note.js
+++ 383-ransom-note/ransom-note.js
@@ -6,7 +6,7 @@
 var canConstruct = function (ransomNote, magazine) {
     // Looping into the magazine string
     for (const char of magazine) {
-        // Replacing every character found in ransomNote similar to magazine with ""
+        // Replacing every character found in ransomNote similar to magazine with blank string("")
         ransomNote = ransomNote.replace(char, "");
     }
     // If ransomNote is blank it means we can form ransomNote with magazine so return TRUE
